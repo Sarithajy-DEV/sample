a447027b882955d315c3bf74ba98f5280d332a6b
Sarithajy-DEV <sarithajaganathan@gmail.com>
2025-05-28 14:20:04 +0530

    initial commit to branch_2

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Branch Main</h1>
+      <h1>Branch 2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
