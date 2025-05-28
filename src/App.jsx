73723199cbb2c5457ce78007dad5579963092d5f
Sarithajy-DEV <sarithajaganathan@gmail.com>
2025-05-28 14:35:10 +0530

    second commit branch develop

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Branch 5</h1>
-      <h1>develop</h1>
+      <h1>Branch Develop</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
